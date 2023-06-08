8c6fbfa7d8757aa32e42171612ca07954995c1b0
Lionel Francis <lionelishmael@gmail.com>
2023-06-07 22:03:39 -0500

    update onboarding

--- api/controllers/onboardingController.js
+++ api/controllers/onboardingController.js
@@ -1,18 +1,26 @@
 const User = require('../../model/User');
-const Companies = require('../../model/Company');
+const Company = require('../../model/Company');
 var Odoo = require('async-odoo-xmlrpc');
 
 
 exports.getOnboarding = async (req, res) => {
 
 	console.log('Post Request: Onboarding Users')
-	console.table(req.table);
+	console.log(req.body)
+
+	const currentDate = new Date();
+
+	const year = currentDate.getFullYear();
+	const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+	const day = String(currentDate.getDate()).padStart(2, '0');
+
+	const formattedDate = `${year}-${month}-${day}`;
 
 	// Onboarding params
-	let date = new Date().toLocaleDateString();
+	let date = formattedDate;
 	let company_name = req.body.company_name;
 	let company_type = req.body.company_type;
-	let tenantname = req.body.tenant 
+	let tenantname = req.body.domain
 	let theme = req.body.theme;
 	let brandcolor = req.body.colors;
 	let subscription = req.body.subscription;
@@ -44,11 +52,20 @@ exports.getOnboarding = async (req, res) => {
     		{is_company: true, is_published: true, is_public: true, name: company_name}
     	]);
 
+    	let domain = req.body.domain+'.'+'imarketplace.world'
     	let company_id = await odoo.execute_kw("res.company", "create", [
-    		{ 'account_opening_date': date, 'active': true, name:  req.body.company_name}
+    		{   'account_opening_date': date, 
+	    		'active': true, 
+	    		name: req.body.company_name, 
+	    		partner_id: partner, 
+	    		website: domain,
+	    		email: req.body.email,
+	    		country: req.body.country,
+	    		state: req.body.state,
+	    		phone: req.body.phone,
+	    		currency_id: 1
+    	    }
     	]);
-
-    	
 
     	const save_user = new User ({
     		firstname: req.body.firstname,
@@ -67,12 +84,13 @@ exports.getOnboarding = async (req, res) => {
     		company_id: company_id,
     		company_name: company_name,
     		company_type: company_type,
-    		tenantname: tenantname,
+    		subdomain: tenantname,
     		theme: theme,
-    		logo: req.body,logo,
+    		phone: req.body.phone,
+    		logo: req.body.logo,
     		brandcolor: brandcolor,
     		subscription: subscription,
-    		country: req.body.contry,
+    		country: req.body.country,
     		city: req.body.city,
     		state: req.body.state
     	})
