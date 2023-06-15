3eba77361d36a66bfd4f3e22ff5b1d166d0cd6dd
Viktoh-Ikenna <viktoh675@gmail.com>
2023-06-15 04:53:00 +0100

    fixed

--- api/routes/onboarding.js
+++ api/routes/onboarding.js
@@ -5,5 +5,7 @@ const auth = require("../../config/auth");
 const asyncHandler = require("../../config/asyncHandler");
 
 router.post("", auth, asyncHandler(onboardingController.getOnboarding)); // Initiates the onboarding process and creates a new onboarding record.
+router.post("/domain/verify", auth, asyncHandler(onboardingController.verifyDomainName)); // Initiates the onboarding process and creates a new onboarding record.
+router.post("/companyname/verify", auth, asyncHandler(onboardingController.verifyCompanyName)); // Initiates the onboarding process and creates a new onboarding record.
 
 module.exports = router;
