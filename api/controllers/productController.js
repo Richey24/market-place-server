5592f46cf0c19d9de7a982456f0b8a8a195a9249
Viktoh-Ikenna <viktoh675@gmail.com>
2023-07-10 18:22:46 +0100

    fixed issues

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,27 +1,32 @@
-var Odoo = require('async-odoo-xmlrpc');
-const Company = require('../../model/Company');
-const { addProduct, getFeaturedProducts } = require('../../services/product.service');
+var Odoo = require("async-odoo-xmlrpc");
+const Company = require("../../model/Company");
+const { addProduct, getFeaturedProducts } = require("../../services/product.service");
 
 exports.getProducts = async (req, res) => {
+     console.log("GET /api/products");
+     var odoo = new Odoo({
+          url: "http://104.43.252.217/",
+          port: 80,
+          db: "bitnami_odoo",
+          username: "user@example.com",
+          password: "850g6dHsX1TQ",
+     });
 
-	console.log("GET /api/products");
-	var odoo = new Odoo({
-		url: 'http://104.43.252.217/', port: 80, db: 'bitnami_odoo',
-		username: 'user@example.com',
-		password: '850g6dHsX1TQ'
-	});
+     try {
+          await odoo.connect();
+          console.log("Connect to Odoo XML-RPC - api/products");
 
-	try {
-		await odoo.connect();
-		console.log("Connect to Odoo XML-RPC - api/products");
-
-		let products = await odoo.execute_kw('product.template', 'search_read', [[['type', '=', 'consu']]], { 'fields': ['name', 'public_categ_ids'] })
-		res.status(201).json({ products });
-
-	} catch (e) {
-		console.error("Error when try connect Odoo XML-RPC.", e);
-	}
-}
+          let products = await odoo.execute_kw(
+               "product.template",
+               "search_read",
+               [[["type", "=", "consu"]]],
+               { fields: ["name", "public_categ_ids"] },
+          );
+          res.status(201).json({ products });
+     } catch (e) {
+          console.error("Error when try connect Odoo XML-RPC.", e);
+     }
+};
 
 /**
  * This function will get featured products for the landing page
@@ -29,128 +34,151 @@ exports.getProducts = async (req, res) => {
  * @param  {[type]} res [description]
  * @return {[type]}     [description]
  */
-exports.getFeaturedProducts = async ( req, res) => {
+exports.getFeaturedProducts = async (req, res) => {
+     console.log("GET  api/products/featured");
 
-	console.log('GET  api/products/featured')
+     let user = req.userData;
+     let company_id = 1;
 
-	let user = req.userData;
-	let company_id = 1;
+     var odoo = new Odoo({
+          url: "http://104.43.252.217/",
+          port: 80,
+          db: "bitnami_odoo",
+          username: "user@example.com",
+          password: "850g6dHsX1TQ",
+     });
 
-	var odoo = new Odoo({
-		url: 'http://104.43.252.217/', port: 80, db: 'bitnami_odoo',
-		username: 'user@example.com',
-		password: '850g6dHsX1TQ'
-	})
+     let params = {
+          odoo: odoo,
+          promo: req.body,
+          user: user,
+     };
 
-	let params = {
-		odoo: odoo,
-		promo: req.body,
-		user: user
-	}
-
-	const products = await getFeaturedProducts(params);
-	res.status(201).json({products})
-}
+     const products = await getFeaturedProducts(params);
+     res.status(201).json({ products });
+};
 
 exports.filterProducts = async (req, res) => {
+     var odoo = new Odoo({
+          url: "http://104.43.252.217/",
+          port: 80,
+          db: "bitnami_odoo",
+          username: "user@example.com",
+          password: "850g6dHsX1TQ",
+     });
 
-	var odoo = new Odoo({
-		url: 'http://104.43.252.217/', port: 80, db: 'bitnami_odoo',
-		username: 'user@example.com',
-		password: '850g6dHsX1TQ'
-	});
+     const category = req.body.category_id;
+     const offset = 5;
+     const page = 0;
 
-	const category = req.body.category_id;
-	const offset = 5;
-	const page = 0;
+     try {
+          await odoo.connect();
 
-	try {
-		await odoo.connect();
+          if (category === null) {
+               let products = await odoo.execute_kw("product.product", "search_read", [
+                    [["type", "=", "consu"]],
+                    [
+                         "name",
+                         "list_price",
+                         "image_512",
+                         "categ_id",
+                         "rating_avg",
+                         "rating_count",
+                         "website_url",
+                         "public_categ_ids",
+                         "website_meta_keywords",
+                    ], // Fields
+                    0,
+                    5, // Offset, Limit
+               ]);
+               res.status(201).json({ products });
+          } else {
+               let products = await odoo.execute_kw("product.product", "search_read", [
+                    [
+                         ["type", "=", "consu"],
+                         ["public_categ_ids", "=", Number(category)],
+                    ],
+                    [
+                         "name",
+                         "list_price",
+                         "image_512",
+                         "categ_id",
+                         "rating_avg",
+                         "rating_count",
+                         "website_url",
+                         "public_categ_ids",
+                         "website_meta_keywords",
+                    ], // Fields
+                    0,
+                    5, // Offset, Limit
+               ]);
 
-		if (category === null) {	
-			let products = await odoo.execute_kw('product.product', 'search_read', [
-				[['type', '=', 'consu']]
-				, ['name', 'list_price', 'image_512', 'categ_id', 'rating_avg', 'rating_count', 'website_url', 'public_categ_ids', 'website_meta_keywords'] // Fields
-				, 0, 5 // Offset, Limit
-			]);
-			res.status(201).json({ products });
-		} else {
-
-			let products = await odoo.execute_kw('product.product', 'search_read', [
-				[['type', '=', 'consu'], ['public_categ_ids', '=', Number(category)]]
-				, ['name', 'list_price', 'image_512', 'categ_id', 'rating_avg', 'rating_count', 'website_url', 'public_categ_ids', 'website_meta_keywords'] // Fields
-				, 0, 5 // Offset, Limit
-			]);
-
-			res.status(201).json({ products });
-		}
-
-	} catch (e) {
-		console.error("Error when try connect Odoo XML-RPC.", e);
-	}
-}
+               res.status(201).json({ products });
+          }
+     } catch (e) {
+          console.error("Error when try connect Odoo XML-RPC.", e);
+     }
+};
 
 exports.productDetails = async (req, res) => {
+     console.log(" GET /api/details");
 
-	console.log(" GET /api/details");
+     const productId = req.params.id;
 
+     try {
+          await odoo.connect();
 
-	const productId = req.params.id
+          console.log("Connect to odoo XML-RPC is successed.");
 
-	try {
-		await odoo.connect();
+          let id = await odoo.execute_kw("product.template", "search", [[["id", "=", productId]]]);
 
-		console.log("Connect to odoo XML-RPC is successed.");
-
-		let id = await odoo.execute_kw('product.template', 'search', [
-			[['id', '=', productId]]]);
-
-		let products = await odoo.execute_kw('product.template', 'read', [id]);
-		res.status(201).json(products);
-
-	} catch (e) {
-		console.error("Error when try connect Odoo XML-RPC.", e);
-	}
-}
+          let products = await odoo.execute_kw("product.template", "read", [id]);
+          res.status(201).json(products);
+     } catch (e) {
+          console.error("Error when try connect Odoo XML-RPC.", e);
+     }
+};
 
 exports.wishlistProduct = async (req, res) => {
+     console.log(" GET /api/details");
 
-	console.log(" GET /api/details");
+     const productId = req.params.id;
 
-	const productId = req.params.id
-	
-	try {
-		await odoo.connect();
+     try {
+          await odoo.connect();
 
-		console.log("Connect to odoo XML-RPC is successed.");
+          console.log("Connect to odoo XML-RPC is successed.");
 
-		let id = await odoo.execute_kw('product.template', 'search', [
-			[['id', '=', productId]]]);
+          let id = await odoo.execute_kw("product.template", "search", [[["id", "=", productId]]]);
 
-		let products = await odoo.execute_kw('product.wishlist', 'read', [id]);
-		res.status(201).json(products);
-
-	} catch (e) {
-		console.error("Error when try connect Odoo XML-RPC.", e);
-	}
-}
+          let products = await odoo.execute_kw("product.wishlist", "read", [id]);
+          res.status(201).json(products);
+     } catch (e) {
+          console.error("Error when try connect Odoo XML-RPC.", e);
+     }
+};
 
 exports.createProduct = async (req, res) => {
-    
-    let user = req.userData;
-    
-	var odoo = new Odoo({
-		url: 'http://104.43.252.217/', port: 80, db: 'bitnami_odoo',
-		username: 'user@example.com',
-		password: '850g6dHsX1TQ'
-	});
+     let user = req.userData;
+     try {
+          var odoo = new Odoo({
+               url: "http://104.43.252.217/",
+               port: 80,
+               db: "bitnami_odoo",
+               username: "user@example.com",
+               password: "850g6dHsX1TQ",
+          });
 
-	let params = {
-		odoo: odoo,
-		product: req.body,
-		// user: user
-	}
+          let params = {
+               odoo: odoo,
+               product: req.body,
+               // user: user
+          };
 
-	const product = await addProduct(params)
-	res.status(201).json({ product })
-}
+          const product = await addProduct({ ...params, user });
+          console.log(product);
+          res.status(201).json({ product });
+     } catch (err) {
+          res.status(400).json({ err });
+     }
+};
--- services/product.service.js
+++ services/product.service.js
@@ -1,96 +1,104 @@
-const Company = require('../model/Company')
+const Company = require("../model/Company");
+const User = require("../model/User");
 
 const unitOfMeasure = async (odoo) => {
-	try {
-		await odoo.connect();
-		console.log('Get unit of mesasure')
-		let uom =  await odoo.execute_kw('uom.uom', 'search_read', [])
-	    return { uom }
-	} catch ( e ) {
-		console.error('Error when trying to connect to odoo xml-rpc');
-	}
-}
+     try {
+          await odoo.connect();
+          console.log("Get unit of mesasure");
+          let uom = await odoo.execute_kw("uom.uom", "search_read", []);
+          return { uom };
+     } catch (e) {
+          console.error("Error when trying to connect to odoo xml-rpc");
+     }
+};
 
 /**
  * This function get feature products
  * @param  {[type]} params [description]
  * @return {[type]}        [description]
  */
-const getFeaturedProducts = async ( params ) => {
-    console.log("GET /api/products");
-	try {
-		await params.odoo.connect();
-		let products = await params.odoo.execute_kw('product.template', 'search_read', [
-			[['type', '=', 'consu']] 
-			, ['name', 'list_price', 'description_sale', 'categ_id', 'id', 'website_url']
-				, 0, 8 
-			]);
-		return products;
-	} catch (e) {
-		console.error("Error when try connect Odoo XML-RPC.", e);
-	}
-}
+const getFeaturedProducts = async (params) => {
+     console.log("GET /api/products");
+     try {
+          await params.odoo.connect();
+          let products = await params.odoo.execute_kw("product.template", "search_read", [
+               [["type", "=", "consu"]],
+               ["name", "list_price", "description_sale", "categ_id", "id", "website_url"],
+               0,
+               8,
+          ]);
+          return products;
+     } catch (e) {
+          console.error("Error when try connect Odoo XML-RPC.", e);
+     }
+};
 
-const addProduct = async (  params ) => {
-	try {
-		await params.odoo.connect();
-		let product = await params.odoo.execute_kw('product.template', 'create', [
-			{
-	    		base_unit_count: params.product.qty, 
-	    		categ_id: params.product.category_id, 
-	    		list_price: params.product.list_price, 
-	    		standard_price: params.product.standard_price, 
-	    		name: params.product.name,
-	    		// image: params.product.image,
-	    		uom_name: params.product.uom_name,
-	    		display_name: params.product.name,
-	    		// product_variant_ids: 1,
-	    		website_published: params.product.published
-			}
-		]);
-		return await product
-	} catch (e) {
-		 console.error("Error when try connect Odoo XML-RPC.", e);
-	}
-}
+const addProduct = async (params) => {
+     try {
+          const user = await User.findById(params.user._id).populate({
+               path: "company",
+               options: { virtuals: true },
+          });
+          console.log(params);
+          if (user) {
+               await params.odoo.connect();
+               let product = await params.odoo.execute_kw("product.template", "create", [
+                    {
+                         base_unit_count: 2,
+                         categ_id: params.product.category,
+                         //  standard_price: params.product.list_price,
+                         standard_price: params.product.standard_price,
+                         name: params.product.title,
+                         // image: params.product.image,
+                         //  uom_name: params.product.uom_name,
+                         display_name: params.product.title,
+                         // product_variant_ids: 1,
+                         //  website_published: params.product.published,
+                         //  company_id: user.company.company_id,
+                    },
+               ]);
+               return await product;
+          } else {
+               throw "Invalid Login";
+          }
+     } catch (e) {
+          throw ("Error when try connect Odoo XML-RPC.", e);
+     }
+};
 
-const updateProduct = async ( params ) => {
-	try {
-		await params.odoo.connect();
-		let product = await odoo.execute_kw('product.template', 'write', [
-        [params.product_id]
-	        , {
-				base_unit_count: params.product.qty, 
-	    		categ_id: params.product.category_id, 
-	    		list_price: params.product.list_price, 
-	    		standard_price: params.product.standard_price, 
-	    		name: params.product.name,
-	    		// image: params.product.image,
-	    		uom_name: params.product.uom_name,
-	    		display_name: params.product.name,
-	    		// product_variant_ids: 1,
-	    		website_published: params.product.published
-			 }
-	    ]);
-	    return product;
-	} catch(e) {
-	  console.error("Error when try connect Odoo XML-RPC.", e);
-	}
-}
-
+const updateProduct = async (params) => {
+     try {
+          await params.odoo.connect();
+          let product = await odoo.execute_kw("product.template", "write", [
+               [params.product_id],
+               {
+                    base_unit_count: params.product.qty,
+                    categ_id: params.product.category_id,
+                    list_price: params.product.list_price,
+                    standard_price: params.product.standard_price,
+                    name: params.product.name,
+                    // image: params.product.image,
+                    uom_name: params.product.uom_name,
+                    display_name: params.product.name,
+                    // product_variant_ids: 1,
+                    website_published: params.product.published,
+               },
+          ]);
+          return product;
+     } catch (e) {
+          console.error("Error when try connect Odoo XML-RPC.", e);
+     }
+};
 
 /**
  * This function delete a user product
  * @param  {[array]} product_id [The id of the product that has been seleected]
  * @return {[productID]}        [Return the id of the product]
  */
-const deleteProduct = async ( params ) => {
-
-}
+const deleteProduct = async (params) => {};
 
 module.exports = {
-  addProduct,
-  unitOfMeasure,
-  getFeaturedProducts
+     addProduct,
+     unitOfMeasure,
+     getFeaturedProducts,
 };
-
