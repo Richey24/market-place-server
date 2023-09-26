4d5c8dbf05b8cd2f8ac29348dbb8462c234c8fef
Rejoice <uahomorejoice@gmail.com>
2023-09-26 16:53:37 +0100

    promotion

--- api/controllers/promotionController.js
+++ api/controllers/promotionController.js
@@ -22,11 +22,19 @@ const Promotion = require('../../model/Promotion');
  * @return {[type]}     [description]
  */
 exports.getPromotions = async (req, res) => {
-
-	let company_id = req.params.company_id;
-
-	const promos = await getPromotion(company_id)
-	res.status(201).json({ promos })
+	try {
+		let company_id = req.params.company_id;
+		if (!company_id) {
+			return res.status(400).json({ message: "Company ID is required" })
+		}
+		const promo = await Promotion.findOne({ company_id: company_id })
+		if (!promo) {
+			return res.status(400).json({ message: "No Promotion Found with this company ID" })
+		}
+		res.status(201).json(promo)
+	} catch (error) {
+		res.status(500).json({ message: "An error occured" })
+	}
 }
 
 /**
@@ -48,29 +56,23 @@ exports.createPromotions = async (req, res) => {
 		const promos = await Promotion.create(body);
 		res.status(201).json(promos)
 	} catch (error) {
-		res.status(201).json({ message: "An error occured" })
+		res.status(500).json({ message: "An error occured" })
 	}
 }
 
 exports.updatePromotions = async (req, res) => {
+	try {
 
-	let user = req.userData;
-
-
-	var odoo = new Odoo({
-		url: 'http://104.43.252.217/', port: 80, db: 'bitnami_odoo',
-		username: 'user@example.com',
-		password: '850g6dHsX1TQ'
-	});
-
-	let params = {
-		id: req.params.id,
-		promo: req.body,
-		user: user
+		const id = req.params.id
+		const body = req.body
+		if (!id) {
+			return res.status(400).json({ message: "ID is required" })
+		}
+		await Promotion.findByIdAndUpdate(id, body)
+		res.status(201).json({ message: "Updated Successfully" })
+	} catch (error) {
+		res.status(500).json({ message: "An error occured" })
 	}
-
-	const promos = await updatePromotion(params);
-	res.status(201).json({ promos })
 }
 
 /**
