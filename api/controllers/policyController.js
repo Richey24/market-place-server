9f3b7a91fbfbeb728645ecbd5efb72498dc9ac07
olaiya <olaiyabasit@yahoo.com>
2023-12-29 17:54:24 +0100

    fixed

--- api/controllers/policyController.js
+++ api/controllers/policyController.js
@@ -1,9 +1,30 @@
 const Policy = require("../../model/Policy");
 
-exports.createPolicy = async (req, res) => {
-     const { siteId, policy_type, content } = req.body;
+exports.getPolicyBySiteId = async (req, res) => {
+     const { siteId } = req.body;
 
      try {
+          if (!siteId) {
+               return res.status(400).json({ error: "Site ID is required in the request body" });
+          }
+
+          const sitePolicies = await Policy.findOne({ site_id: siteId });
+
+          if (!sitePolicies) {
+               return res.status(404).json({ error: "Site not found or no policies exist" });
+          }
+
+          return res.status(200).json({ policies: sitePolicies.policies });
+     } catch (error) {
+          console.error("Error getting policy:", error);
+          return res.status(500).json({ error: "Internal server error" });
+     }
+};
+
+exports.createPolicy = async (req, res) => {
+     try {
+          const { siteId, policy_type, content } = req.body;
+
           if (!siteId) {
                return res.status(400).json({ error: "Site ID is required in the request body" });
           }
@@ -15,6 +36,10 @@ exports.createPolicy = async (req, res) => {
                     site_id: siteId,
                     policies: [],
                });
+          }
+
+          if (!Policy.schema.path("policies.0.policy_type").enumValues.includes(policy_type)) {
+               return res.status(400).json({ error: "Invalid policy_type value" });
           }
 
           const existingPolicy = sitePolicies.policies.find(
@@ -68,6 +93,37 @@ exports.updatePolicy = async (req, res) => {
           return res.status(200).json({ message: "Policy updated successfully" });
      } catch (error) {
           console.error("Error updating policy:", error);
+          return res.status(500).json({ error: "Internal server error" });
+     }
+};
+
+exports.getPolicyByType = async (req, res) => {
+     try {
+          const { siteId, policy_type } = req.body;
+
+          if (!siteId || !policy_type) {
+               return res
+                    .status(400)
+                    .json({ error: "Site ID and Policy Type are required in the request body" });
+          }
+
+          const sitePolicies = await Policy.findOne({ site_id: siteId });
+
+          if (!sitePolicies) {
+               return res.status(404).json({ error: "Site not found or no policies exist" });
+          }
+
+          const matchingPolicy = sitePolicies.policies.find(
+               (policy) => policy.policy_type === policy_type,
+          );
+
+          if (!matchingPolicy) {
+               return res.status(404).json({ error: "Policy not found for the specified type" });
+          }
+
+          return res.status(200).json({ policy: matchingPolicy });
+     } catch (error) {
+          console.error("Error getting policy by type:", error);
           return res.status(500).json({ error: "Internal server error" });
      }
 };
--- api/routes/policy.js
+++ api/routes/policy.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const policyController = require("../controllers/policyController");
 
+router.get("/", policyController.getPolicyBySiteId);
 router.post("/create", policyController.createPolicy);
 router.put("/update", policyController.updatePolicy);
+router.get("/get-policy-by-type", policyController.getPolicyByType);
 
 module.exports = router;
