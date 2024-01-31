40c14388466b2d76ee62516f294a0117050d1391
Viktoh-Ikenna <viktoh675@gmail.com>
2024-01-31 03:32:47 +0100

    fixed

--- api/routes/service.js
+++ api/routes/service.js
@@ -33,11 +33,11 @@ router.post("/cat/get/first", getFirstSubCategory);
 router.post("/cat/get/second", getSecondSubCategory);
 router.delete("/delete/:id", serviceController.deleteService);
 
-router.post("/order/start/:orderId", serviceOrder.start);
-router.post("/order/hold/:orderId", serviceOrder.hold);
-router.post("/order/continue/:orderId", serviceOrder.continue);
-router.post("/order/deliver/:orderId", serviceOrder.deliver);
-router.post("/order/admin/mark-as-paid/:orderId", serviceOrder.markAsPaid);
+router.put("/order/start/:orderId", serviceOrder.start);
+router.put("/order/hold/:orderId", serviceOrder.hold);
+router.put("/order/continue/:orderId", serviceOrder.continue);
+router.put("/order/deliver/:orderId", serviceOrder.deliver);
+router.put("/order/admin/mark-as-paid/:orderId", serviceOrder.markAsPaid);
 router.put("/order/confirm-payment/:orderId", confirmPayment);
 
 router.post("/orders/create", serviceOrder.create);
