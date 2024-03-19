1604d21b26309ad647f127eeb09525135b06c966
Viktoh-Ikenna <viktoh675@gmail.com>
2024-03-19 22:43:00 +0100

    fixed

---
+++ utils/midBannerConfiq.js
@@ -0,0 +1,106 @@
+const bannerImages = require("./bannerImages");
+
+const midBannerConfig = {
+     theme2: [
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 700,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 700,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 700,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 700,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme2"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+     ],
+     theme5: [
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme5"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme5"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+          {
+               title: "",
+               text: "",
+               imageUrl: bannerImages?.["theme5"]?.banner1,
+               link: "/",
+               width: 300,
+               height: 400,
+          },
+     ],
+};
+
+module.exports = midBannerConfig;
