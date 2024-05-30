3f456d88fd29ff78aa2ac80de07a37993ac1036d
Rejoice <uahomorejoice@gmail.com>
2024-05-30 14:30:48 +0100

    fix

--- seed.js
+++ seed.js
@@ -5,6 +5,7 @@ const Service = require("./model/Service");
 const { default: algoliasearch } = require("algoliasearch");
 const { serviceNav, ecommerceNav } = require("./utils/navigation");
 const { sendAdminMessage } = require("./config/helpers");
+const Odoo = require("./config/odoo.connection");
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
@@ -22,7 +23,18 @@ mongoose
 const addNav = async () => {
      try {
           // sendAdminMessage("uahomorejoice@gmail.com", "Rejoice", "testing the new email")
+          await Odoo.connect();
 
+          const orderLines = await Odoo.execute_kw(
+               "sale.order.line",
+               "search_read",
+               [],
+               {
+                    fields: ["product_id", "product_uom_qty", "price_unit"],
+               },
+          );
+          console.log(orderLines);
+          // await Odoo.execute_kw("sale.order.line", "unlink", [[Number(33)]]);
           console.log("worked");
      } catch (error) {
           console.log(error);
